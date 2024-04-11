a05d4d21efb3e4dc7d29736812a6baeebeeaee0e
italo pessan <italopessan@gmail.com>
2024-04-10 23:18:07 -0300

    testes iniciais projeto console acessando banco de dados

---
+++ Teste-Console/Program.cs
@@ -0,0 +1,11 @@
+﻿using Teste_Console.Context;
+
+var context = new DataContext();
+
+var cidades = context.Cidades.ToList();
+foreach (var produto in cidades)
+{
+    Console.WriteLine($"Cidades: {produto.Cidades}");
+}
+
+Console.ReadLine();
